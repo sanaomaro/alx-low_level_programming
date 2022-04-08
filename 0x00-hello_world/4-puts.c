8e725e71826888228fc2e21b3574f13594a2f1bb
sanaomaro <somarya.020@gmail.com>
2022-04-08 02:41:08 -0700

    editing white spaces

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/*
- * main - function begins the starting point of a function1
+/**
+ * main - function begins the entry point of the program
  * Return: function ends , returning zero 
-*/
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
