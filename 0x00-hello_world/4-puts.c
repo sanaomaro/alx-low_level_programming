e5ea4181912706699b1f212870bdba1c894c5807
sanaomaro <somarya.020@gmail.com>
2022-04-08 01:33:03 -0700

    using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
+/**
+ *
+ * *main - function begins the starting point of a function
+ *
+ * *
+ *
+ * * Return: function ends , returning zero
+ *
+ * */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");				return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 }
