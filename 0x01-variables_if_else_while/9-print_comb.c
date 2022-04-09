623d2596aaebfc1fc6218a067249465fe30d65b4
sanaomaro <somarya.020@gmail.com>
2022-04-09 00:05:08 -0700

    white spaces

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,21 +3,19 @@
  * main - Entry point, this program print numbers with spaces
  * Return: 0
  */
-
 int main(void)
-
 {
 	int i;
 
 	for (i = '0'; i <= '9';i++)
+	{
+		putchar(i);
+		if (i != '9')
 		{
-			putchar(i);
-			if (i != '9')
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			putchar(',');
+			putchar(' ');
 		}
+	}
 	putchar('\n');
 
 	return (0);
