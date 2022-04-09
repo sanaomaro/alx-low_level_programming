b8e2823b9cf82591bd26dc04ac2081b114b582ae
sanaomaro <somarya.020@gmail.com>
2022-04-08 19:07:08 -0700

    printing lower case alphabet using putchar function

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+		int no;
+
+		for (no =1 ; no <= 26; no++)
+			putchar(no);
+			printf("\n");
+		return (0);
+
+}
