1fdc3a85151ea295e2c0e7e727f40534f4083a9a
sanaomaro <somarya.020@gmail.com>
2022-04-08 21:15:20 -0700

    printing numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - the starting point a program that prints numbers using putchar
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+		int no;
+		for (no = '0'; no <= '9'; no++)
+		{
+			putchar(no);
+		}
+
+		putchar('\n');
+
+		return (0);
+
+}
