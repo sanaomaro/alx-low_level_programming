d1525b2fc7d65d2a86737473ecd4272e3413ba70
sanaomaro <somarya.020@gmail.com>
2022-04-09 00:01:46 -0700

    because of trailing whitespace suspect indenting adding a space

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,15 +10,14 @@ int main(void)
 	int i;
 
 	for (i = '0'; i <= '9';i++)
-	{
-		putchar(i);
-		if (i != '9')
+		{
+			putchar(i);
+			if (i != '9')
 			{
 				putchar(',');
 				putchar(' ');
 			}
-	
-	}
+		}
 	putchar('\n');
 
 	return (0);
