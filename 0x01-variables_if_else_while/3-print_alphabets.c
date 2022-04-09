067d255dae64b1355938e2c3ad99fc8a9722ebc8
sanaomaro <somarya.020@gmail.com>
2022-04-08 19:36:17 -0700

    printing uppercase and lowecase using putchar

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+		char charalpha;
+
+		for (charalpha = 'a'; charalpha <= 'z'; charalpha++)
+			putchar(charalpha);
+		for (charalpha = 'A'; charalpha <= 'Z'; charalpha+)
+			putchar(charalpha);
+			putchar('\n');
+		return (0);
+}
