36ca27ac1b374ad14e8910ddc1754e77ba71221e
sanaomaro <somarya.020@gmail.com>
2022-04-08 19:24:58 -0700

    editing trialing space error for a second time

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
 		char lower_char;
 	
 		for (lower_char = 'a'; lower_char <= 'z'; lower_char++)
 			putchar(lower_char);
-			
+
 		printf("\n");
 		return (0);
-
 }
