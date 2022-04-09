e995f204efb997e512a38b1039dfafbcd2d5a695
sanaomaro <somarya.020@gmail.com>
2022-04-08 23:57:34 -0700

    edit because I printed combinations of two digits from 00 to 99 while it was asked from 0 to 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,21 +8,18 @@ int main(void)
 
 {
 	int i;
-	int j;
 
 	for (i = '0'; i <= '9';i++)
 	{
-		for(j='0'; j <='9'; j++)
+		putchar(i);
+		if (i != '9')
 			{
-				putchar(i);
-				putchar(j);
-				if (i != '9' && j != '9')
-					{
-						putchar(',');
-						putchar(' ');
-					}
+				putchar(',');
+				putchar(' ');
 			}
-	}
 	
+	}
+	putchar('\n');
+
 	return (0);
 }
