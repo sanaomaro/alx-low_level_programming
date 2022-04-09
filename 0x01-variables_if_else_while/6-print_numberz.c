c28a8772cad4676a093ee21ed6da8dd312beb6fb
sanaomaro <somarya.020@gmail.com>
2022-04-08 21:17:06 -0700

    fixing blankline warning

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,13 +3,14 @@
 #include <time.h>
 /**
  * main - the starting point a program that prints numbers using putchar
- * Return: Always 0 (Success)
+ * Return: Always return 0 
  */
 
 int main(void)
 
 {
 		int no;
+
 		for (no = '0'; no <= '9'; no++)
 		{
 			putchar(no);
