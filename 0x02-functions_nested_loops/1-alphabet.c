7c2330598b5f0c3436bd6182e888e31aa68442c2
sanaomaro <somarya.020@gmail.com>
2022-04-12 17:25:04 -0700

    printing alphabets then a newline

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * printing alphabet in lower case - starting point
+ * Return:0
+ */
+void print_alphabet(void)
+{
+	char my_cahr = 'a';
+	while (my_char<= 'z')
+	{
+		_putchar(my_char);
+		my_char++;
+	}
+	_putchar('\n');
+}
