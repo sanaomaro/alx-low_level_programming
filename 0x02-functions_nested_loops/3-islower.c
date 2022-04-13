888016c552723fd7635850ae7bb52fc28518c9c9
sanaomaro <somarya.020@gmail.com>
2022-04-12 18:14:09 -0700

    returns 1 if it is a lwercase, zero otherwise

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * _islower - check @c letter if it is a lwer case returns 1 otherwise returns 0
+ * Return:0 or 1
+ */
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
