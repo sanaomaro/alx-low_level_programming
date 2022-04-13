8596f6fb7fc5e829d78bd7825df65274b5e82735
sanaomaro <somarya.020@gmail.com>
2022-04-12 18:41:29 -0700

    reurns abd=solutr value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _abs - computes abs value
+ * @n :integer value - the input to be checked *
+ * Return:0 or 1
+ */
+int _abs(int n)
+
+{
+	return (n >= 0 ? n : n * -1);
+}
