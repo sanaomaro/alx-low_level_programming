7982daebbca16af9d9a8744373a485b38a1a15bc
sanaomaro <somarya.020@gmail.com>
2022-04-12 18:17:33 -0700

    editing the comments because @c was not declared

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * _islower - check @c letter if it is a lwer case returns 1 otherwise returns 0
+ * _islower - check @c, if its a lowercase returns 1 otherwise returns 0
+ * @c :character that needs to be checked
  * Return:0 or 1
  */
 int _islower(int c)
