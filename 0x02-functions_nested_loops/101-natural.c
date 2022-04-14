881416607c93c1a0d70c3afa14a569444637cc39
sanaomaro <somarya.020@gmail.com>
2022-04-14 09:32:26 -0700

    fixing spaces and betty style

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/* main - to printsum of all numbers under 1024 that are multiple of 3 or 5
+ * returm 0 success
+ */
 int main(void)
 {
 	int i;
 	int sum = 0;
 
-	for(i = 1; i < 1024; i++)
+	for (i = 1; i < 1024; i++)
 	{
-		if((i % 3 == 0) || (i % 5 == 0))
+		if ((i % 3 == 0) || (i % 5 == 0))
 		{
 		sum = sum + i;
 		}
-		printf("%i\n", sum);
-
 	}
 	printf("%i\n", sum);
-	return 0;
+	return (0);
 }
