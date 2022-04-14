de9a1b026110f284363e6b4a18a1f7ecc7d12d1b
sanaomaro <somarya.020@gmail.com>
2022-04-14 04:22:01 -0700

    a function that prints all minuts per day for a 24 hours colck

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include main.h"
+
+/**
+ * jack_bauer - function that prints all minutes in a day, 24 hours system
+ * Return: 24 day's hours by minute for each line*
+ */
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');									_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');							_putchar('\n');
+		}
+	}
+}
