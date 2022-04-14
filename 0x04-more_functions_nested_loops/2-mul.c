57535c31ce26a5b678c4b9e63296414427403fd9
sanaomaro <somarya.020@gmail.com>
2022-04-14 16:00:59 -0700

    multiplication function of two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *  mul - function that multiplies two integers
+ *  @x: first input
+ *  @y: second input
+ *  Return: multiplication result 
+ */
+
+int mul(int x, int y)
+
+{
+	return (x * y);
+
+}
