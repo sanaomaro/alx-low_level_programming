f259f74788bd9d891552422fcff5e4bb2676650e
sanaomaro <somarya.020@gmail.com>
2022-04-14 16:04:59 -0700

    fixing typops and betty style

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,14 +1,11 @@
 #include "main.h"
 /**
  *  mul - function that multiplies two integers
- *  @x: first input
- *  @y: second input
- *  Return: multiplication result 
+ *  @x: the first no
+ *  @y: the second no
+ *  Return: 0 
  */
-
 int mul(int x, int y)
-
 {
 	return (x * y);
-
 }
