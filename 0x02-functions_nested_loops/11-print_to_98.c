9b794aa60f7c6f8f467a2f594b1dfd0077898114
sanaomaro <somarya.020@gmail.com>
2022-04-14 08:14:08 -0700

    printing numbers from n to 98 or from 98 to n

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * print_to_98 - a function that prints all numbers from n to 98
+ * in both cses when @n is > or < 98
+ * @n: the starting number
+ * Return: Always 0
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
+	}
+	else if (n > 98)
+		{
+			while (n >= 98)
+			{
+				printf("%d", n);
+				if (n != 98)
+				{
+					printf(", ");
+				}
+				n--;
+			}
+		}
+	else
+		{
+			printf("98");
+		}
+	printf("\n");
+}
