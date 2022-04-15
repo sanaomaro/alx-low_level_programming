b299ad26857dd39bfe75adaef5841a1825896fda
sanaomaro <somarya.020@gmail.com>
2022-04-15 14:19:03 -0700

    printing numbers depedning on the fact if they are multiple of 5 or 3 or not

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the numbers from 1 to 100, followed by a new line
+ * numbers that are ltiples of three prints Fizz instead of the number
+ * if multiples of five prints Buzz
+ * if both print FizzBuzz
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf(" Fizz");
+		} else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf(" Buzz");
+		} else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		} else if (i == 1)
+		{
+			printf("%d", i);
+		} else
+		{
+			printf(" %d", i);
+		}
+	}
+		printf("\n");
+	return (0);
+}
