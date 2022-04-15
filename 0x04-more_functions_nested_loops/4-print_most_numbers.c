2dc8faf6d2a9df2d3143936a635014f66e68f6b1
sanaomaro <somarya.020@gmail.com>
2022-04-15 09:58:59 -0700

    printing numbers from 0 - 9 expect 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - A function prints numbers from 0 to 9 except 2 and 4
+ * Return: nothing
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (!(i == 2 || i == 4))
+			_putchar(i);
+	}
+	_putchar('\n');
+}
