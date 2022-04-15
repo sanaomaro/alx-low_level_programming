d7a44aaf07a5caf0bc6b5aa73650bf2a0270f003
sanaomaro <somarya.020@gmail.com>
2022-04-15 14:47:23 -0700

    fixing typos

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,7 +7,7 @@ void print_triangle(int size)
 {
 	if (size <= 0)
 	{
-		putchar('\n');
+		_putchar('\n');
 	} else
 	{
 		int i, j;
@@ -15,13 +15,13 @@ void print_triangle(int size)
 		{
 			for (j = i; j < size; j++)
 			{
-				putchar('.');
+				_putchar('.');
 			}
 			for (j = 1; j <= i; j++)
 			{
-				putchar('#');
+				_putchar('#');
 			}
-			putchar('\n');
+			_putchar('\n');
 		}
 	}
 }
