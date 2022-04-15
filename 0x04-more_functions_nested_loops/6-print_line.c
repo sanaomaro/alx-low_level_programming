815fe256bf2846b46cebb84d01db965539d4d41c
sanaomaro <somarya.020@gmail.com>
2022-04-15 10:42:39 -0700

    print lines for n times

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/* print_line - printn straight lines for @n times
+ * @n: the number of times to print a line
+ * Return: 0
+ */
+void print_line(int n)
+{
+	int i;
+	
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
