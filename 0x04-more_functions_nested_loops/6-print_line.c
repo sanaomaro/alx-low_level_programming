16e51098f79fdf9a4206fd89ff5dadd8ebed0a71
sanaomaro <somarya.020@gmail.com>
2022-04-16 03:36:38 -0700

    fixing doc problem

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,8 @@
 #include "main.h"
-/* print_line - printn straight lines for @n times
- * @n: the number of times to print a line
- * Return: 0
+/**
+ ** print_line - printn straight lines for @n times
+ ** @n: the number of times to print a line
+ ** Return: 0
  */
 void print_line(int n)
 {
