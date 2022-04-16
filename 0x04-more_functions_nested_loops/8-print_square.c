6d91f8cf74b7ed8e0c406a3b66ab9039a723c8f8
sanaomaro <somarya.020@gmail.com>
2022-04-16 03:56:26 -0700

    suspect code indent for conditional statement problem fixing

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,23 +1,23 @@
 #include "main.h"
 /**
- * print_square - A function who print a square using #
- * @size: squareside 
- * Return: nothing
+ ** print_square - A function who print a square using #
+ ** @size: squareside
+ ** Return: nothing
  */
-
 void print_square(int size)
 {
 	int i, j;
 	int n = size;
-	
+
 	if (n > 0)
 	{
+
 		for (j = 0; j < n; j++)
 		{
 			for (i = 0; i < n; i++)
-				{
-					_putchar('#');
-				}
+			{
+				_putchar('#');
+			}
 			_putchar('\n');
 		}
 	}
