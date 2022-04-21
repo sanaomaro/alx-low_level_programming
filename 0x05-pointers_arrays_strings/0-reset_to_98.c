684d62497a7967b58b50c72df70f313632b08b1d
sanaomaro <somarya.020@gmail.com>
2022-04-20 17:59:14 -0700

    assign pointer to int parameter

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - assign pointer to int, update its value that points to 98
+ * @n: input
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
