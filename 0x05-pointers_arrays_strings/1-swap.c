0d3df2692f1fc413243c6829d7b2ca043f11f493
sanaomaro <89298616+sanaomaro@users.noreply.github.com>
2022-04-21 03:06:12 +0200

    Update 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - swaping tow integers values
+ * @a: input 1
+ * @b: input 2
+ * Return: integers
+ */
+void swap_int(int *a, int *b)
+{
+	int n;
+
+	n = *a;
+	*a = *b;
+	*b = n;
+}
