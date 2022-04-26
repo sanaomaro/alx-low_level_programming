07e478baa462a357d231cc7e544a35c6c237b699
sanaomaro <somarya.020@gmail.com>
2022-04-26 13:09:29 -0700

    comparing wo strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcmp - Comparing two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: negative int if s1 < s2, 0 if equal, and positive int if s1 > s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (0);
+}
