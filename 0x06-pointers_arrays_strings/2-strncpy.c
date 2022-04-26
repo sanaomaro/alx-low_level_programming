b7e539725df16d4bf1ade6e0f4887422a9fd2b5e
sanaomaro <somarya.020@gmail.com>
2022-04-26 12:20:57 -0700

    copies string problem fixing

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,13 +9,18 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
+	i = 0;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	while (i < n && src[i] != '\0')
+	{
 		dest[i] = src[i];
-
-	for (; n > i; i++)
+		i++;
+	}
+	i = i;
+	while (i < n)
+	{
 		dest[i] = '\0';
-
+		i++;
+	}
 	return (dest);
-
 }
