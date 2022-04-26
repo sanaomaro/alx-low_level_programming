a8796aee32e4e8675cfbced4ace909ed19f9f304
sanaomaro <somarya.020@gmail.com>
2022-04-26 16:00:55 -0700

    filling the memory with a constant byte b

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memcpy - function fills memory with a constant byte.
+ * @s: input pointer s
+ * @b: input constant byte b
+ * @n: input n bytes area to be filled
+ * Return: Always 0 (Success)
+  */
+char *_memcpy(char *s, char *b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b[i];
+
+	return (s);
+
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+my readme for more pointers to pointers example
