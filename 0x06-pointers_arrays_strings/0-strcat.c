bc723eb02a4ea7c49e52d84aee96e05881e2577f
sanaomaro <somarya.020@gmail.com>
2022-04-26 10:38:16 -0700

    concatecations problems

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,21 +7,25 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int a;
-	int b;
-	a = 0;
+	int i, j, a, b = 0;
 
-	while (dest[a] != 0)
+	while (1)
 	{
-		a++;
+		if (*(dest + i) == '\0')
+			break;
+			i++;
 	}
-	b = 0;
-	while (src[b] != 0)
+	while (1)
 	{
-		dest[a] = dest[a] + src[b];
-		a++;
+		if (*(src + j) == '\0')
+			break;
+			j++;
+	}
+	for (a = i; a < i + j; a++)
+	{
+		*(dest + a) = *(src + b);
 		b++;
 	}
+	*(dest + a) = '\0';
 	return (dest);
-
 }
