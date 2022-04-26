5ee9cf3e5fe1f0b06805e078e0317250ef1aa756
sanaomaro <somarya.020@gmail.com>
2022-04-26 16:07:01 -0700

    filling the memory with a constant byte b

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,7 +6,7 @@
  * @n: input n bytes area to be filled
  * Return: Always 0 (Success)
   */
-char *_memcpy(char *s, char *b, unsigned int n)
+char *_memset(char *s, char *b, unsigned int n)
 {
 	unsigned int i;
 
