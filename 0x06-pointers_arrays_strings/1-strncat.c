1687d27e78aeec2c63459346b8c5d38165dc689f
sanaomaro <somarya.020@gmail.com>
2022-04-26 13:58:10 -0700

    concatenation two strings with \0 end

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,34 +8,19 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0, j = 0, a, b = 0;
+	int i;
+	int d = 0;
 
-	while (1)
+	while(dest[d])
+		d++;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		if (*(dest + i) == '\0')
-			break;
-			i++;
+		dest[d + 1] =src[i];
 	}
-	while (1)
-	{
-		if (*(src + j) == '\0')
-			break;
-			j++;
-	}
-	if (n >= j)
-	{
-		;
-	}
-	else
-	{
-		j = n;
-	}
-	for (a = i; a < i + j; a++)
-	{
-		*(dest + a) = *(src + b);
-		b++;
-	}
-	*(dest + a) = '\0';
+
+	dest[d + 1] = '\0';
+
 	return (dest);
 
 }
