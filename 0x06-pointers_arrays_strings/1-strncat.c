e968fdf93efb6a96f58bd54fde93cd126786a66b
sanaomaro <somarya.020@gmail.com>
2022-04-27 13:10:06 -0700

    fixing betty style

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,12 +11,12 @@ char *_strncat(char *dest, char *src, int n)
 	int i;
 	int d = 0;
 
-	while(dest[d])
+	while (dest[d])
 		d++;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		dest[d + 1] =src[i];
+		dest[d + 1] = src[i];
 		dest[d + 1] = '\0';
 	}
 	return (dest);
