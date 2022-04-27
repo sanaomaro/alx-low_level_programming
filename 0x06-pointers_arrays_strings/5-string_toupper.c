b7a2e015b7b3fa3f459ed0a135c052ea901a185b
sanaomaro <somarya.020@gmail.com>
2022-04-27 15:22:54 -0700

    fixing betty style

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * string_toupper - changeing letters of array from lowercase to uppercase
- * @c: array string
+ * @s: array string
  * Return: uppercase string output
   */
 
@@ -11,7 +11,7 @@ char *string_toupper(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if(s[i] >= 96 && s[i] <= 123)
+		if (s[i] >= 96 && s[i] <= 123)
 			s[i] -= 32;
 	}
 	return (s);
