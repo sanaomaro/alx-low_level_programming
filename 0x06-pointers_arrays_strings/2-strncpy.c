7900e397762dfa75534f788a4bd375da63fa4d60
sanaomaro <somarya.020@gmail.com>
2022-04-27 13:31:47 -0700

    deleting i = i, was written mistakenly

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,6 +9,7 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
+
 	i = 0;
 
 	while (i < n && src[i] != '\0')
@@ -16,7 +17,8 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[i] = src[i];
 		i++;
 	}
-	i = i;
+
+
 	while (i < n)
 	{
 		dest[i] = '\0';
