7942fb764940144fe59fe33a4db8234edfa84e0a
sanaomaro <somarya.020@gmail.com>
2022-04-27 13:12:43 -0700

    fixing description for parameters

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _strncat - concatenates two strings
- * @des: first string
+ * @dest: first string
  * @src: Second string
  * @n: number of bytes from src
  * Return: Pointer to the resulting string dest
@@ -11,7 +11,7 @@ char *_strncat(char *dest, char *src, int n)
 	int i;
 	int d = 0;
 
-	while (dest[d])
+	while (dest[d] != '\0')
 		d++;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
