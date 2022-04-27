8a8ae36e0bf3df894ec39260986b3042cafbb2c2
sanaomaro <somarya.020@gmail.com>
2022-04-27 16:28:40 -0700

    rotate 13 sypher

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string in rot13 cypher
+ * @s: string to be encoded or rotated
+ * Return: the resulting string after rotating 13 charachters
+ */
+char *rot13(char *s)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		s[i] = s[i] + 13;
+	}
+	return (s);
+}
